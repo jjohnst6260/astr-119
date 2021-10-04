a459dd9f28431ff01a45dec8a974914ef20d50d9
jjohnst6260 <91391546+jjohnst6260@users.noreply.github.com>
2021-10-04 12:21:37 -0700

    Add files via upload

---
+++ astr-119-session-3/test_module.py
@@ -0,0 +1,4 @@
+# test_module.py
+
+def hello_world():
+	print("Hello world, we made a module!")
---
+++ astr-119-session-3/useful_modules.py
@@ -0,0 +1,7 @@
+import numpy as np    # the numpy library
+import matplotlib.pyplot as plt   # matplotlib's pyplot
+import sys      # gives access to C-like sys library
+import os    # gives access to operating system
+
+print(sys.argv)  # command line arguments
+print(os.getcwd())  # print current working directory
